61292be409ef8993366bbb91541e900cf28d2684
Abhibagad <abhishekbagad9@gmail.com>
2022-12-20 16:13:27 +0530

    Sample

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using BinraySerchTree;
+
+namespace LinkedListExample
+{
+    class Progarm
+    {
+        public static void Main(string[] args)
+        {
+            Tree tree = new Tree();
+            tree.Insert(56);
+            tree.Insert(30);
+            tree.Insert(70);
+            tree.Display(tree.ReturnRoot());
+        }
+    }
+}
---
+++ Tree.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinraySerchTree
+{
+  public class Tree
+    {
+       
+        public Node root;
+
+        public Tree()
+        {
+            root = null;
+        }
+        public Node ReturnRoot()
+        {
+            return root;
+        }
+        public void Insert(int id)
+        {
+            Node newNode = new Node(id);
+            if(root == null)
+            {
+                root = newNode;
+            }
+            else
+            {
+                Node current = root;
+                Node parent;
+                while(true)
+                {
+                    parent = current;
+                    if(id < current.item)
+                    {
+                        current = current.leftc;
+                        if(current == null)
+                        {
+                            parent.leftc = newNode;
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        current = current.rightc;
+                        if(current == null)
+                        {
+                            parent.rightc = newNode;
+                            return;
+                        }
+                    }
+                }
+                
+
+            }
+            
+        }
+        public void Display(Node Root)
+        {
+            if (Root != null)
+            {
+                Console.WriteLine(Root.item + "|");
+                Display(Root.leftc);
+                Display(Root.rightc);
+            }
+        }
+
+
+    }
+}
